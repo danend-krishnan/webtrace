5dd9b4a5666bfe1edb5fe22335ecd2b328c02446
root <root@localhost.localdomain>
2025-01-23 22:56:33 +0530

    Middlewares and Authentications

---
+++ middlewares/adminMiddleware.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const Router = express.Router;
+const { adminModel } = require("./../db");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_SEC="t-f6Va9!k*@v1k$UrFV_0JsQ1H%x43DMl!0&amp;"
+const bcrypt = require("bcrypt");
+
+async function adminMiddleware(req, res, next)
+{
+    const email = req.body.email;
+    const password = req.body.password;
+    let admin = await adminModel.findOne({
+        email: email 
+    })
+    
+    if(!admin){
+        res.json({
+            msg: "WHO ARE YOU"
+        })
+    }
+    if(admin){const passwordcheck = await bcrypt.compare(password, admin.password);
+    if(passwordcheck){
+        const token = jwt.sign({
+            id: admin._id
+        }, JWT_ADMIN_SEC);
+        req.userId=token.id;
+        req.token=token;
+    next();
+    }if(!passwordcheck){
+        res.status(403).json({
+            msg: "Who are you"
+        })
+    }}
+}
+
+module.exports = {
+    adminMiddleware: adminMiddleware
+}
--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -2,37 +2,41 @@ const express = require("express");
 const Router = express.Router;
 const { adminModel } = require("./../db");
 const jwt = require("jsonwebtoken");
-const JWT_ADMIN_SEC="t-f6Va9!k*@v1k$UrFV_0JsQ1H%x43DMl!0&amp;"
 const bcrypt = require("bcrypt");
+const {adminMiddleware} = require("./../middlewares/adminMiddleware");
 
 const adminRouter = Router();
 //adminRouter.use(adminMiddleware);
 
-adminRouter.get("/login", async(req, res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
-    let admin = await adminModel.findOne({
-        email: email
-    })
-    if(!admin){
-        res.json({
-            msg: "WHO ARE YOU"
-        })
-    }
-    const passwordcheck = bcrypt.compare(password, admin.password);
-    if(passwordcheck){
-        const token = jwt.sign({
-            id: admin._id
-        }, JWT_ADMIN_SEC);
+adminRouter.get("/login", adminMiddleware, async(req, res)=>{
+    // const email = req.body.email;
+    // const password = req.body.password;
+    // let admin = await adminModel.findOne({
+    //     email: email
+    // })
+    // if(!admin){
+    //     res.json({
+    //         msg: "WHO ARE YOU"
+    //     })
+    // }
+    // const passwordcheck = bcrypt.compare(password, admin.password);
+    // if(passwordcheck){
+    //     const token = jwt.sign({
+    //         id: admin._id
+    //     }, JWT_ADMIN_SEC);
+    // res.json({
+    //     msg: "admin logged in",
+    //     token: token
+    // })}else{
+    //     res.status(403).json({
+    //         msg: "Who are you"
+    //     })
+    // }
     res.json({
-        msg: "admin logged in",
-        token: token
-    })}else{
-        res.status(403).json({
-            msg: "Who are you"
-        })
-    }
-})
+        msg: "WELCOME ADMIN",
+        token: req.token
+    });
+});
 
 adminRouter.post("/register", async(req, res)=>{
     const email = req.body.email;
@@ -40,7 +44,7 @@ adminRouter.post("/register", async(req, res)=>{
     try{
     await adminModel.create({ 
         email: email,
-        password: bcrypt.hash(password, 10)
+        password: await bcrypt.hash(password, 10)
     })}catch(e){} //Rabitholes
     res.json({
         msg: "ADMIN REG SUCCESS"
