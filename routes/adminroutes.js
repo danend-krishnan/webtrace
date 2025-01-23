90ab5219213c9b86e8466e516b1f618ce933c238
root <root@localhost.localdomain>
2025-01-23 22:03:59 +0530

    Validations and Jwt

--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const Router = express.Router;
-const { adminModel } = require("./../db")
+const { adminModel } = require("./../db");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_SEC="t-f6Va9!k*@v1k$UrFV_0JsQ1H%x43DMl!0&amp;"
+const bcrypt = require("bcrypt");
 
 const adminRouter = Router();
 //adminRouter.use(adminMiddleware);
 
-adminRouter.get("/login", (req, res)=>{
-    res.json({
-        msg: "admin"
+adminRouter.get("/login", async(req, res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    let admin = await adminModel.findOne({
+        email: email
     })
+    if(!admin){
+        res.json({
+            msg: "WHO ARE YOU"
+        })
+    }
+    const passwordcheck = bcrypt.compare(password, admin.password);
+    if(passwordcheck){
+        const token = jwt.sign({
+            id: admin._id
+        }, JWT_ADMIN_SEC);
+    res.json({
+        msg: "admin logged in",
+        token: token
+    })}else{
+        res.status(403).json({
+            msg: "Who are you"
+        })
+    }
 })
 
 adminRouter.post("/register", async(req, res)=>{
@@ -17,7 +40,7 @@ adminRouter.post("/register", async(req, res)=>{
     try{
     await adminModel.create({ 
         email: email,
-        password: password
+        password: bcrypt.hash(password, 10)
     })}catch(e){} //Rabitholes
     res.json({
         msg: "ADMIN REG SUCCESS"
