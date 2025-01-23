aad4a8484676d1b9e2e804f398aa5e708c921052
root <root@localhost.localdomain>
2025-01-23 18:46:13 +0530

    Routes Created

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const {adminRouter} = require("./routes/adminroutes")
 
-app.use("/admin", adminRouter)
+app.use("/admin", adminRouter);
 
 app.listen(3000);
--- routes/adminroutes.js
+++ routes/adminroutes.js
@@ -2,11 +2,16 @@ const express = require("express");
 const Router = express.Router;
 
 const adminRouter = Router();
+adminRouter.use(adminMiddleware);
 
 adminRouter.get("/login", (req, res)=>{
     res.json({
         msg: "admin"
     })
+})
+
+adminRouter.post("/register", (req, res)=>{
+    
 })
 
 module.exports = {
