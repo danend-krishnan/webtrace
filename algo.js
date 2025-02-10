7dec083145eaac4151bb7310b0cc89d91f349b41
Danendkrishnan <141625041+danend-krishnan@users.noreply.github.com>
2025-02-10 12:21:43 +0530

    nasty stuff done and dusted

--- algo.js
+++ algo.js
@@ -1,5 +1,6 @@
 //PENDING FOR THE DEV, HE/SHE SHOULD TOUCH IT...!
 const fs = require('fs');
+const compareStrings = require('compare-strings');
 
 
 async function readFileContent(filePath, callback) {
@@ -17,18 +18,30 @@ async function compareFiles(file1, file2) {
     await readFileContent(file1, (file1Content) => {
         console.log("reaached here 1")
          readFileContent(file2, (file2Content) => { //UMM AWAIT NEEDED HERE
-            console.log(file1Content)
+            console.log(typeof(file1Content))
             console.log()
-            console.log(file2Content)
-            if (file1Content === file2Content) {
-                console.log('Match found!');
-            } else {
-                console.log('No match!');
+            console.log(typeof(file2Content))
+            // console.log(file2Content)
+            // console.log(file1Content === file2Content)
+            // if (file1Content.toLowerCase() === file2Content.toLowerCase()) {
+            //     console.log('Match found!');
+            // } else {
+            //     console.log('No match!');
+            // }
+            const result = compareStrings(file1Content,file2Content);
+            if(result==1){
+                console.log("Nasty Copy")
+            }else if(result>=0.5){
+                console.log("Nasty Hate words Detected")
+            }else{
+                console.log("No nasty Hate words buddy")
             }
         });
     });
 
 }
+
+
 
 
 module.exports={
