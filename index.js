29859fe2a371341ab414598dceada4912b285f0f
root <root@localhost.localdomain>
2025-02-10 22:23:34 +0530

    Nasty Dan

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use("/admin", adminRouter);
 
 async function main(){
 await mongoose.connect("mongodb+srv://webtrace:Sn6l5ATchttCCZuS@cluster0.vx69r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-app.listen(3000);
+app.listen(3001);
 console.log("Nasty Connection Started")
 }
 main(); 
