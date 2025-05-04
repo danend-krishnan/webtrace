9b5aa2c041c55872e3190d6e02ae7cc8b2de1e5a
krish <krish@localhost.localdomain>
2025-05-04 18:20:48 +0530

    test: test 2

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use("/admin", adminRouter);
 async function main(){
 await mongoose.connect("mongodb+srv://webtrace:Sn6l5ATchttCCZuS@cluster0.vx69r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 app.listen(3001);
-console.log(" Connection Started")
+console.log(" Connection Staarted")
 }
 main(); 
